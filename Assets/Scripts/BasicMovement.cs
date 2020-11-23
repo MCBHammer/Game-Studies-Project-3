89328eee4cae7cee09c26075d7d678d11bfbbb8c
MCBHammer <armanaznavour@yahoo.com>
2020-11-22 22:22:52 -0600

    Sprites and Lateral Movement

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    [SerializeField] float speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(x, y);
+    }
+
+    private void Walk(Vector2 movement)
+    {
+        rb.velocity = new Vector2(movement.x * speed, rb.velocity.y);
+    }
+}
